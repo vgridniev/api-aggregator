898227e71745d0d68b88a4a03d3a5e73d8a28a08
vgridniev <99885281+vgridniev@users.noreply.github.com>
2024-07-04 23:18:43 +0300

    Prepare for Heroku deployment with PostgreSQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, Logger } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CountriesModule } from './countries/countries.module';
@@ -6,32 +6,21 @@ import { LeaguesModule } from './leagues/leagues.module';
 import { CountryEntity } from './entities/country.entity';
 import { LeagueEntity } from './entities/league.entity';
 
-const logger = new Logger('AppModule');
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            envFilePath: ['.env.local', '.env', '.env.production'],
+            envFilePath: ['.env.local', '.env', '.env.prod'],
         }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => {
-                logger.log(`POSTGRES_HOST: ${configService.get<string>('POSTGRES_HOST')}`);
-                logger.log(`POSTGRES_PORT: ${configService.get<string>('POSTGRES_PORT')}`);
-                logger.log(`POSTGRES_USER: ${configService.get<string>('POSTGRES_USER')}`);
-                logger.log(`POSTGRES_PASSWORD: ${configService.get<string>('POSTGRES_PASSWORD')}`);
-                logger.log(`POSTGRES_DB: ${configService.get<string>('POSTGRES_DB')}`);
                 return {
                     type: 'postgres',
-                    host: configService.get<string>('POSTGRES_HOST'),
-                    port: parseInt(
-                        configService.get<string>('POSTGRES_PORT'),
-                        10,
-                    ),
-                    username: configService.get<string>('POSTGRES_USER'),
-                    password: configService.get<string>('POSTGRES_PASSWORD'),
-                    database: configService.get<string>('POSTGRES_DB'),
+                    url:
+                        process.env.DATABASE_URL ||
+                        configService.get<string>('DATABASE_URL'),
                     entities: [CountryEntity, LeagueEntity],
                     synchronize: true,
                 };
