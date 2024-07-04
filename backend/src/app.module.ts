36308db032476d780dfdd3b691522bdc321a17c4
vgridniev <99885281+vgridniev@users.noreply.github.com>
2024-07-04 20:43:15 +0300

    .

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CountriesModule } from './countries/countries.module';
@@ -6,16 +6,22 @@ import { LeaguesModule } from './leagues/leagues.module';
 import { CountryEntity } from './entities/country.entity';
 import { LeagueEntity } from './entities/league.entity';
 
+const logger = new Logger('AppModule');
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            envFilePath: ['.env.local', '.env'],
+            envFilePath: ['.env.local', '.env', '.env.production'],
         }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => {
+                logger.log(`POSTGRES_HOST: ${configService.get<string>('POSTGRES_HOST')}`);
+                logger.log(`POSTGRES_PORT: ${configService.get<string>('POSTGRES_PORT')}`);
+                logger.log(`POSTGRES_USER: ${configService.get<string>('POSTGRES_USER')}`);
+                logger.log(`POSTGRES_PASSWORD: ${configService.get<string>('POSTGRES_PASSWORD')}`);
+                logger.log(`POSTGRES_DB: ${configService.get<string>('POSTGRES_DB')}`);
                 return {
                     type: 'postgres',
                     host: configService.get<string>('POSTGRES_HOST'),
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
-    const port = configService.get<number>('PORT') || 4000; // Default to 4000 if PORT is not set
+    const port = configService.get<number>('POSTGRES_PORT') || 4000; // Default to 4000 if PORT is not set
+
+    const logger = new Logger('Bootstrap');
+    logger.log(`Connecting to database at ${configService.get<string>('DATABASE_URL')}`);
+    logger.log(`Starting server on port ${port}`);
+
     await app.listen(port);
     console.log(`Application is running on: ${await app.getUrl()}`);
+    logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
