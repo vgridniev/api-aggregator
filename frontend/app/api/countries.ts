ea7bfdb51f5c0bee7925c430b13de0cbf5e03ad6
vgridniev <99885281+vgridniev@users.noreply.github.com>
2024-07-06 22:32:42 +0300

    logs

--- frontend/app/api/countries.ts
+++ frontend/app/api/countries.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { ApiResponse } from '../../interfaces/country';
 
+console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+
 export const fetchCountries = async (): Promise<ApiResponse> => {
     const response = await axios.get<ApiResponse>(`${process.env.NEXT_PUBLIC_API_URL}/countries`);
     return response.data;
--- frontend/app/api/leagues.ts
+++ frontend/app/api/leagues.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { ApiResponse } from '../../interfaces/league';
 
+console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
 
 export const fetchLeaguesByCountry = async (country: string): Promise<ApiResponse> => {
-    const response = await axios.get(`${process.env.API_URL}/leagues?country=${country}`);
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/leagues?country=${country}`);
     return response.data;
 };
