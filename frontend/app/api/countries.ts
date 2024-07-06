346c29f07ac56083f7699a33f988e790cfa065f9
vgridniev <99885281+vgridniev@users.noreply.github.com>
2024-07-06 22:12:10 +0300

    update

--- frontend/app/api/countries.ts
+++ frontend/app/api/countries.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { ApiResponse } from '../../interfaces/country';
 
 export const fetchCountries = async (): Promise<ApiResponse> => {
-    const response = await axios.get<ApiResponse>(`${process.env.API_URL}/countries`);
+    const response = await axios.get<ApiResponse>(`${process.env.NEXT_PUBLIC_API_URL}/countries`);
     return response.data;
 };
