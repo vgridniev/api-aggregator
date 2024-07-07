15c2fe58a1ad15ec546aecf5f69f4d7bff278bdf
vgridniev <99885281+vgridniev@users.noreply.github.com>
2024-07-07 10:55:39 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const configService = app.get(ConfigService);
 
     // Enable CORS
     app.enableCors({
@@ -18,7 +16,7 @@ async function bootstrap() {
         methods: 'GET,HEAD',
         credentials: true,
     });
-    const port = configService.get<number>('POSTGRES_PORT') || 4000; // Default to 4000 if PORT is not set
+    const port = process.env.PORT || 4000;
 
     const logger = new Logger('Bootstrap');
 
